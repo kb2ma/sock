ea3747811af09b04b3b86cd5409efa8efec7d665
Kaspar Schleiser <kaspar@schleiser.de>
2016-10-27 12:14:19 +0200

    nanocoap: client: adapt to changed nanocoap_get()

--- nanocoap/client.c
+++ nanocoap/client.c
@@ -7,7 +7,10 @@
 #include <unistd.h>
 
 #include "net/sock/udp.h"
+#include "net/sock/util.h"
+
 #include "nanocoap.h"
+#include "nanocoap_sock.h"
 
 int main(int argc, char *argv[])
 {
@@ -18,7 +21,27 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    ssize_t res = coap_get(argv[1], buf, sizeof(buf));
+    char *url = argv[1];
+    sock_udp_ep_t remote;
+
+    char hostport[SOCK_HOSTPORT_MAXLEN] = {0};
+    char urlpath[SOCK_URLPATH_MAXLEN] = {0};
+
+    if (strncmp(url, "coap://", 7)) {
+        return 1;
+    }
+
+    ssize_t res = sock_urlsplit(url, hostport, urlpath);
+    if (res) {
+        return 1;
+    }
+
+    res = sock_str2ep(&remote, hostport);
+    if (res) {
+        return 1;
+    }
+
+    res = nanocoap_get(&remote, urlpath, buf, sizeof(buf));
     if (res <= 0) {
         fprintf(stderr, "error %zi\n", res);
         return 1;
