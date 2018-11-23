fc2c4bffce3d65b0aa8f14981048e3118c79a456
Ken Bannister <kb2ma@runbox.com>
2018-11-23 06:19:24 -0500

    Add /riot/value handler

--- nanocoap/handler.c
+++ nanocoap/handler.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+#include "fmt.h"
 #include "net/nanocoap.h"
+
+/* internal value that can be read/written via CoAP */
+static uint8_t internal_value = 0;
 
 ssize_t _test_handler(coap_pkt_t* pkt, uint8_t *buf, size_t len, void *context)
 {
     (void)context;
 
-    printf("_test_handler()\n");
-    printf("coap pkt parsed. code=%u detail=%u payload_len=%u, len=%u 0x%02x\n",
-            coap_get_code_class(pkt),
-            coap_get_code_detail(pkt),
-            pkt->payload_len, (unsigned)len, pkt->hdr->code);
-
     const char payload[] = "1234";
     return coap_reply_simple(pkt, COAP_CODE_205, buf, len, COAP_FORMAT_TEXT, (uint8_t*)payload, 4);
 }
 
+static ssize_t _riot_value_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
+{
+    (void) context;
+
+    ssize_t p = 0;
+    char rsp[16];
+    unsigned code = COAP_CODE_EMPTY;
+
+    /* read coap method type in packet */
+    unsigned method_flag = coap_method2flag(coap_get_code_detail(pkt));
+
+    switch(method_flag) {
+    case COAP_GET:
+        /* write the response buffer with the internal value */
+        p += fmt_u32_dec(rsp, internal_value);
+        code = COAP_CODE_205;
+        break;
+    case COAP_PUT:
+    case COAP_POST:
+    {
+        /* convert the payload to an integer and update the internal value */
+        char payload[16] = { 0 };
+        memcpy(payload, (char*)pkt->payload, pkt->payload_len);
+        internal_value = strtol(payload, NULL, 10);
+        code = COAP_CODE_CHANGED;
+    }
+    }
+
+    return coap_reply_simple(pkt, code, buf, len,
+            COAP_FORMAT_TEXT, (uint8_t*)rsp, p);
+}
+
 const coap_resource_t coap_resources[] = {
     COAP_WELL_KNOWN_CORE_DEFAULT_HANDLER,
+    { "/riot/value", COAP_GET | COAP_PUT | COAP_POST, _riot_value_handler, NULL },
     { "/test", COAP_GET, _test_handler, NULL },
 };
 
